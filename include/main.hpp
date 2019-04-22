d4f45b149b28cb7e598e735e58386d643073f31c
shadowninja108 <shadowninja0108@gmail.com>
2019-04-21 20:28:50 -0700

    Bruh

--- include/main.hpp
+++ include/main.hpp
@@ -1,8 +1,7 @@
 #include "svc.h"
 #include "types.h"
 #include "agl/lyr/renderinfo.h"
-#include "Lp/Sys/gfxlayer2d.h"
 #include "agl/utl/devtools.h"
 #include "ModuleObject.hpp"
 
-void render(Lp::Sys::GfxLayer2D *layer, agl::lyr::RenderInfo *renderInfo);
+void render(agl::lyr::Layer *layer, agl::lyr::RenderInfo *renderInfo);
--- source/main.cpp
+++ source/main.cpp
@@ -2,30 +2,30 @@
 __attribute__((section(".bss"))) rtld::ModuleObject __nx_module_runtime; // to appease rtld
 
 // hook for Lp::Sys::GfxLayer2D::drawUser
-void render(Lp::Sys::GfxLayer2D *layer, agl::lyr::RenderInfo *renderInfo)
+void render(agl::lyr::Layer *layer, agl::lyr::RenderInfo *renderInfo)
 {
     agl::DrawContext* drawContext = renderInfo->drawContext;
-     
-    sead::Vector3<float>* p1 = new sead::Vector3<float>();
-    p1->mX = 0.0;
-    p1->mY = 0.0;
-    p1->mZ = 0.0;
-    sead::Vector3<float>* p2 = new sead::Vector3<float>();
-    p2->mX = 0.0;
-    p2->mY = 127.0;
-    p2->mZ = 0.0;
-    sead::Vector3<float>* p3 = new sead::Vector3<float>();
-    p3->mX = 127.0;
-    p3->mY = 0.0;
-    p3->mZ = 0.0;
 
-    sead::Color4f* c = new sead::Color4f();
-    c->r = 255.0;
-    c->g = 255.0;
-    c->b = 255.0;
-    c->a = 255.0;
+    sead::Vector3<float> p1;
+    p1.mX = 0.0;
+    p1.mY = 0.0;
+    p1.mZ = 0.0;
+    sead::Vector3<float> p2;
+    p2.mX = 0.0;
+    p2.mY = 127.0;
+    p2.mZ = 0.0;
+    sead::Vector3<float> p3;
+    p3.mX = 127.0;
+    p3.mY = 0.0;
+    p3.mZ = 0.0;
 
-    agl::utl::DevTools::drawTriangleImm(drawContext, *p1, *p2, *p3, *c);
+    sead::Color4f c;
+    c.r = 255.0;
+    c.g = 255.0;
+    c.b = 255.0;
+    c.a = 255.0;
+
+    //agl::utl::DevTools::drawTriangleImm(drawContext, p1, p2, p3, c);
 }
 
 int main(int argc, char **argv) 
