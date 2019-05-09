5d178832289e13b5294ce1d126c3c1701b2c5e33
shibbo <xarf98@yahoo.com>
2019-05-09 18:26:10 -0400

    Expand on Cmn::Def::Gear

--- include/Cmn/Def/Gear.h
+++ include/Cmn/Def/Gear.h
@@ -4,14 +4,23 @@
 
 namespace Cmn
 {
-    namespace Def{
-        class Gear {
-            public:
-            _DWORD id;
-            _QWORD qword4;
-            _QWORD qwordC;
-            _QWORD qword14;
-            _DWORD dword1C;
+    namespace Def
+    {
+        class Gear
+        {
+        public:
+            Gear();
+
+            void resetExpAndExSkillAll();
+
+            s32 mGearID; // _0
+            u32 _4;
+            u32 _8;
+            u32 _C;
+            u32 _10; // ExSkillID
+            u32 _14; // ^^
+            u32 _18; // ^^
+            u32 _1C;
         };
     };
 };
