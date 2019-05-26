d858f2bc2aed2ea1a4ac270bf2b326a0ce2e4d66
3096 <contact@the3096.com>
2019-05-26 06:00:46 -0700

    more 310 symbols & update+fix genLinkerScript.py

--- scripts/genLinkerScript.py
+++ scripts/genLinkerScript.py
@@ -2,17 +2,30 @@
 # Generates a linker script based on a given symbol map and base address offset
 import sys
 
-symbolExceptions = [ "_init", "_fini", "__nnDetailNintendoSdkRuntimeObjectFileRefer", "__nnDetailNintendoSdkRuntimeObjectFile", "__nnDetailNintendoSdkNsoFileRefer" ]
+symbolExceptions = [ "_init",
+					 "_fini",
+					 "__nnDetailNintendoSdkRuntimeObjectFileRefer",
+					 "__nnDetailNintendoSdkRuntimeObjectFile",
+					 "__nnDetailNintendoSdkNsoFileRefer" 
+]
 symbolPrefixes = [ 
 					"Curl", # some curl stuff
 					"curl", # moar curl
-					"_ZG",  # guards
-					"_ZN",  # regular variables
-					"_ZT",  # typeinfo / vtables
-					"_ZZ",
+					# "_ZG",  # guards
+					# "_ZN",  # regular variables
+					# "_ZT",  # typeinfo / vtables
+					# "_ZZ",
+					"_Z",   # literally everything
 					"pfnc_",
 					"mem",
 					"str"
+					"alloc",
+					"malloc",
+					"free",
+					"calloc",
+					"realloc",
+					"aligned_alloc",
+					"malloc_usable_size"
 ]
 
 if len(sys.argv) < 3:
@@ -22,24 +35,23 @@ if len(sys.argv) < 3:
 # open our map file
 with open(sys.argv[1], "r") as f:
 	lines = f.readlines()
-	
-# we need to skip a few lines first
-curLine = 0
 
 # open our new generated linker
 with open("syms.ld", "w") as linker:
-	for line in lines:
-	
-		# skip the first 4 useless lines
-		if curLine < 4:
-			curLine += 1
-			continue
 
+	# we need to skip a few lines first
+	curLine = 0
+	for line in lines:
 		# each line is structured as such:
 		#  00000000:0000007100000160       _fini
 		firstSplit = line.split(" ")
+		if len(firstSplit) <= 1:
+			continue
 		
 		addressSplit = firstSplit[1].split(":")
+		if len(addressSplit) <= 1:
+			continue
+
 		address = addressSplit[1]
 		
 		symbolName = firstSplit[8]
@@ -48,26 +60,24 @@ with open("syms.ld", "w") as linker:
 		# the last few numbers are important, the higher ones are not
 		address = address[9:]
 		address = "0x" + address
-
-		isValid = False
 	
 		# we check to see if this symbol has a prefix that we need to keep
-		for i in range(9):
-			if symbolName.startswith(symbolPrefixes[i]):
-				isValid = True
-				break
-		
-		if symbolName in symbolExceptions:
-			isValid = True
-		
-		if not isValid:
-			continue
+		if (any([symbolName.startswith(symbolPrefix)
+				for symbolPrefix in symbolPrefixes])
+				or symbolName in symbolExceptions):
 
-		# sdk functions have a _0 at the end because they are duplicated due to the originals being imported
-		# so IDA adds a _0 at the end...we need to strip this
-		if "_ZN2nn" in symbolName:
-			symbolName = symbolName.strip("_0")
-		
-		linker.write(f"{symbolName} = {address} - {sys.argv[2]};\n")
+			# skip the first 5 useless lines
+			if curLine < 5:
+				curLine += 1
+				continue
+
+			# sdk functions have a _0 at the end because they are duplicated
+			# due to the originals being imported, so IDA adds a _0 at the
+			# end... we need to strip this
+			if symbolName.endswith("_0"):
+				symbolName = symbolName[:-2]
+			
+			linker.write(
+				"{} = {} - {};\n".format(symbolName, address, sys.argv[2]))
 	
 print("Linker script generated.")
