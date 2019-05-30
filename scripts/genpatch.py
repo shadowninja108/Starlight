eb7d62900da432f65df1c754b6447031c19e5292
3096 <contact@the3096.com>
2019-05-29 21:16:37 -0700

    Implement symbol resolve in genPatch.py

--- scripts/genpatch.py
+++ scripts/genpatch.py
@@ -1,102 +1,161 @@
-import json
-import sys
-
-if len(sys.argv) != 4:
-    print('Usage: ' + sys.argv[0] + ' <definitions file> <patch template> <output file>')
+import os, sys, re, ctypes
 
 class Patch:
-    # offset is an int
-    # contents is a bytes object
-    def __init__(self, offset, contents):
+    def __init__(self, offset, length, content):
         self.offset = offset
-        self.contents = contents
+        self.length = length
+        self.content = content
 
-class PatchList:
-    # name is a string
-    # patches is an array of "Patch"es
-    def __init__(self, name, patches):
-        self.name = name
-        self.patches = patches
+PATCH_DIR = "patches"
+PATCH_EXTENSION = ".slpatch"
 
-# parse definitions
-def_file = open(sys.argv[1], 'r')
-line = def_file.readline().strip()
-defs = {}
-while line:
-    if line.startswith('#'):
-        line = def_file.readline().strip()
-        continue
-    defs[line.partition('=')[0].strip()] = int(line.partition('=')[2].strip(), 16)
-    line = def_file.readline().strip()
-print('defs: ' + str(defs))
-if not 'DIST_FROM_MAIN' in defs:
-    print('Could not find DIST_FROM_MAIN in definitions')
-    sys.exit(1)
+PATCH_CONFIG_DIR = os.path.join(PATCH_DIR, "configs")
+PATCH_CONFIG_EXTENSION = ".config"
 
-# parse patch template
-template_file = open(sys.argv[2], 'r')
-template_json = json.load(template_file)
-print('template: ' + str(template_json))
-if not 'build_id' in template_json:
-    print('Could not find build_id in template')
-    sys.exit(2)
-patchlists = []
-for template_key, template_value in template_json.items():
-    if template_key == 'build_id':
-        build_id = template_value
-        continue
-    if not 'name' in template_value:
-        print('Could not find name in patchlist')
-        sys.exit(3)
-    # everything that's not build_id is treated as a patchlist
-    for patchlist_key, patchlist_value in template_value.items():
-        print(patchlist_value)
-        patches = []
-        # everything that's not name is treated as a patch
-        if patchlist_key == 'name':
-            continue
-        if not 'type' in patchlist_value:
-            print('Could not find type in patch')
-            sys.exit(4)
-        if not 'offset' in patchlist_value:
-            print('Could not find offset in patch')
-            sys.exit(5)
-        if not 'contents' in patchlist_value:
-            print('Could not find contents in patch')
-            sys.exit(6)
-        # python doesn't have switch (case), have fun sifting through the else ifs
-        if patchlist_value['type'] == 'hex_le':
-            # "contents": "0xDEADBEEF" or "contents": "DEADBEEF"
-            # contents are little-endian
-            # patches[patchlist_value['name']] = bytearray.fromhex(patchlist_value['items'].replace('0x', ''))
-            try:
-                patch_offset = int(patchlist_value['offset'].replace('0x', ''), 16)
-            except ValueError:
-                # try and get the offset from defs if it's not hex
-                if not patchlist_value['offset'] in defs:
-                    print('Could not find ' + str(patchlist_value['offset']) + ' in ' + sys.argv[1])
-                    sys.exit(7)
-                patch_offset = defs[patchlist_value['offset']]
-            patches.append(Patch(patch_offset, bytearray.fromhex(patchlist_value['contents'].replace('0x', ''))))
-    patchlists.append(PatchList(template_value['name'], patches))
+NSO_HEADER_LEN = 0x100
+
+buildVersion = None
+patchConfig = {
+    "build_id" : {},
+    "nso_load_addr" : {}
+}
+patchList = {}
+
+mapFile = None
+
+def initConfig():
+    configPath = os.path.join(PATCH_CONFIG_DIR, buildVersion + PATCH_CONFIG_EXTENSION)
+    with open(configPath) as configFile:
+        curConfigName = None
+        for line in configFile:
+            configNameLineMatch = re.match(r'\[(.+)\]', line)
+            if configNameLineMatch:
+                curConfigName = configNameLineMatch.group(1)
+                continue
+
+            if '=' in line:
+                configNSO, configValue = line.split('=', 1)
+                patchConfig[curConfigName][configNSO] = configValue
+
+def getSymAddrFromMap(regexStr, symStr):
+    mapFilePath = os.path.join("build" + buildVersion, "starlight" + buildVersion + ".map")
+    with open(mapFilePath, 'r') as f:
+        mapFile = f.read()
+
+    def getFoundPosAddr(start):
+        regexMatch = re.search(regexStr, mapFile[start:])
+        if not regexMatch:
+            return -1, -1
+
+        foundLinePos = mapFile.rfind('\n', 0, regexMatch.span()[0] + start) + 1
+        foundLineEndPos = mapFile.find('\n', regexMatch.span()[1] + start)
+        foundLine = mapFile[foundLinePos:foundLineEndPos].strip()
+        foundAddr = int(foundLine[:foundLine.find(" ")], 16)
+        
+        return foundLineEndPos, foundAddr
+
+    foundPos, firstFoundAddr = getFoundPosAddr(0)
+    if foundPos == -1:
+        raise "symbol not found"
+
+    # check uniqueness
+    while True:
+        foundPos, moreFoundAddr = getFoundPosAddr(foundPos)
+        if foundPos == -1:
+            break
+
+        if moreFoundAddr != firstFoundAddr:
+            print("genPatch.py error:", symStr, "is ambiguous!")
+            sys.exit(-1)
+
+    # map stores signed address relative to starlight as unsigned?
+    return int(patchConfig["nso_load_addr"]["subsdk0"], 16) + ctypes.c_long(firstFoundAddr).value
+
+def resolveAddress(target, symbolStr):
+    resolvedAddr = 0
+
+    plusSplit = symbolStr.split('+', 1)
+    if len(plusSplit) > 1:
+        symbolStr = plusSplit[0]
+        resolvedAddr += int(plusSplit[1], 16)
+
+    # if symbol is one of the targets
+    if symbolStr in patchConfig["nso_load_addr"]:
+        resolvedAddr += int(patchConfig["nso_load_addr"][symbolStr], 16)
+        return resolvedAddr
+
+    targetSplit = symbolStr.split('!', 1)
+    if len(targetSplit) > 1:
+        target = targetSplit[0]
+        symbolStr = targetSplit[1]
+    else:
+        # if symbol is in starlight
+        funcStr = symbolStr + r'\('
+        try:
+            addrInStarlight = getSymAddrFromMap(funcStr, symbolStr)
+            resolvedAddr +=  + addrInStarlight
+            return resolvedAddr
+        except:
+            pass
+
+    resolvedAddr += int(patchConfig["nso_load_addr"][target], 16)
+
+    # if symbolStr is already address
+    try:
+        resolvedAddr += int(symbolStr, 16)
+        return resolvedAddr
+    except ValueError:
+        pass
+
+    regexStr = r'\w*'.join(re.findall(r'\w+', symbolStr))
+    try:
+        resolvedAddr += getSymAddrFromMap(regexStr, symbolStr)
+    except:
+        print("genPatch.py error: cannot resolve", symbolStr)
+        sys.exit(-1)
+
+    return resolvedAddr
+
+def addPatchFromFile(patchFilePath):
+    PATCH_VERSION_ALL = "all"
+    patchVars = {
+        "version" : None,
+        "target" : "main"
+    }
+
+    with open(patchFilePath) as patchFile:
+        fileLinesIter = iter(patchFile)
+        for line in fileLinesIter:
+            line = line.split('/', 1)[0]
+
+            patchVarLineMatch = re.match(r'\[(.+)\]', line)
+            if patchVarLineMatch:
+                patchVarMatches = re.findall(r'(\w+)=(\w+)', patchVarLineMatch.group(1))
+                for match in patchVarMatches:
+                    patchVars[match[0]] = match[1]
+                continue
+
+            addressSplit = line.split(' ', 1)
+            isMultiPatch = addressSplit[0].endswith(':')
+            if len(addressSplit) < 2 and not isMultiPatch:
+                continue
+            
+            patchAddress = resolveAddress(patchVars["target"],
+                addressSplit[0] if not isMultiPatch else addressSplit[0][:-1])
+            print(addressSplit[0], "resovled to", hex(patchAddress))
+
+            # UNFINISHED
 
 
-# write ipswitch patch
-with open(sys.argv[3], 'w') as pchtxt:
-    pchtxt.write('// This file was automatically generated by genpatch.py\n')
-    pchtxt.write('@little-endian\n')
-    pchtxt.write('@nsobid-' + template_json['build_id'] + '\n')
-    pchtxt.write('@flag offset_shift 0x100\n')
-    for patchlist in patchlists:
-        pchtxt.write('\n')
-        pchtxt.write('// ' + patchlist.name + '\n')
-        pchtxt.write('@enabled\n')
-        for patch in patchlist.patches:
-            offset_hex = hex(patch.offset).replace('0x', '').upper()
-            if len(offset_hex) > 8:
-                print('Offset ' + hex(patch.offset) + ' is too large!')
-                sys.exit(7)
-            pchtxt.write(('0' * (8 - len(offset_hex))) + offset_hex)
-            pchtxt.write(' ')
-            pchtxt.write(''.join('{:02X}'.format(x) for x in patch.contents))
-            pchtxt.write('\n')
+if len(sys.argv) < 2:
+    print('Usage: ' + sys.argv[0] + ' [version]')
+    sys.exit(-1)
+
+buildVersion = sys.argv[1]
+initConfig()
+
+for file in os.listdir(PATCH_DIR):
+    if file.endswith(PATCH_EXTENSION):
+        addPatchFromFile(os.path.join(PATCH_DIR, file))
+
+# UNFINISHED
