3836094432e4b0e9676c9577cc492e3aac91be96
3096 <contact@the3096.com>
2019-06-02 16:39:18 -0700

    genPatch allow separate nso sym maps (sdk patch support)

--- scripts/genPatch.py
+++ scripts/genPatch.py
@@ -13,25 +13,26 @@ PATCH_EXTENSION = ".slpatch"
 
 NSO_HEADER_LEN = 0x100
 PATCH_CONFIG_DIR = os.path.join(PATCH_DIR, "configs")
+PATCH_NSO_MAP_DIR = os.path.join(PATCH_DIR, "maps")
 PATCH_CONFIG_EXTENSION = ".config"
 
 IPS_OUT_DIR_NAME = "starlight_patch_{}"
 IPS_FORMAT = ".ips"
 IPS_HEADER_MAGIC = bytes("IPS32", 'ASCII')
-IPS_EOF_MAGIC = bytes("EEOP", 'ASCII')
+IPS_EOF_MAGIC = bytes("EEOF", 'ASCII')
 
 # globals
 buildVersion = None
 patchConfig = {
     "build_id" : {},
-    "nso_load_addr" : {}
+    "nso_load_addr" : {},
+    "map_file" : {}
 }
 patchList = {}
 
-mapFile = None
-
 def initConfig():
     configPath = os.path.join(PATCH_CONFIG_DIR, buildVersion + PATCH_CONFIG_EXTENSION)
+    # read config file
     with open(configPath) as configFile:
         curConfigName = None
         for line in configFile:
@@ -43,9 +44,23 @@ def initConfig():
 
             if '=' in line:
                 configNSO, configValue = line.split('=', 1)
+                if not configNSO.isalnum():
+                    continue
+                if '+' in configValue:
+                    print("genPatch.py error:", line, "awaits implementation")
+                    sys.exit(-1)
                 patchConfig[curConfigName][configNSO] = configValue
 
-def getSymAddrFromMap(regexStr, symStr):
+    # read nso map files
+    curVerMapDir = os.path.join(PATCH_NSO_MAP_DIR, buildVersion)
+    for file in os.listdir(curVerMapDir):
+        if file.endswith('.map'):
+            with open(os.path.join(curVerMapDir, file), 'r') as f:
+                patchConfig['map_file'][file[:-4]] = f.read()
+
+# returns symbol at it's loaded addr relative to main
+def getSymAddrFromMap(target, regexStr, symStr):
+    mapFile = None
     def getFoundPosAddr(start):
         regexMatch = re.search(regexStr, mapFile[start:])
         if not regexMatch:
@@ -54,13 +69,20 @@ def getSymAddrFromMap(regexStr, symStr):
         foundLinePos = mapFile.rfind('\n', 0, regexMatch.span()[0] + start) + 1
         foundLineEndPos = mapFile.find('\n', regexMatch.span()[1] + start)
         foundLine = mapFile[foundLinePos:foundLineEndPos].strip()
-        foundAddr = int(foundLine[:foundLine.find(" ")], 16)
+        foundAddr = int(foundLine[:foundLine.find(" ")].replace("00000000:00000071", ''), 16)
         
         return foundLineEndPos, foundAddr
 
+    if target in patchConfig['map_file']:
+        mapFile = patchConfig['map_file'][target]
+        loadAddrAdjustment = int(patchConfig["nso_load_addr"][target], 16)
+    else:
+        mapFile = SLMapFile
+        loadAddrAdjustment = int(patchConfig["nso_load_addr"]["subsdk0"], 16)
+
     foundPos, firstFoundAddr = getFoundPosAddr(0)
     if foundPos == -1:
-        raise "symbol not found"
+        raise NameError
 
     # check uniqueness
     while True:
@@ -73,53 +95,56 @@ def getSymAddrFromMap(regexStr, symStr):
             sys.exit(-1)
 
     # map stores signed address relative to starlight as unsigned?
-    return int(patchConfig["nso_load_addr"]["subsdk0"], 16) + ctypes.c_long(firstFoundAddr).value
+    return loadAddrAdjustment + ctypes.c_long(firstFoundAddr).value
 
-def resolveAddress(target, symbolStr):
+def resolveAddressAndTarget(target, symbolStr):
     resolvedAddr = 0
 
+    # resolve + offset
     plusSplit = symbolStr.split('+', 1)
     if len(plusSplit) > 1:
         symbolStr = plusSplit[0]
         resolvedAddr += int(plusSplit[1], 16)
 
-    # if symbol is one of the targets
-    if symbolStr in patchConfig["nso_load_addr"]:
-        resolvedAddr += int(patchConfig["nso_load_addr"][symbolStr], 16)
-        return resolvedAddr
-
+    # resolve different nso target with '!' notation
     targetSplit = symbolStr.split('!', 1)
     if len(targetSplit) > 1:
         target = targetSplit[0]
         symbolStr = targetSplit[1]
     else:
+        # if symbol is one of the nso targets
+        if symbolStr in patchConfig["nso_load_addr"]:
+            target = symbolStr
+            resolvedAddr += int(patchConfig["nso_load_addr"][symbolStr], 16)
+            return target, resolvedAddr
+
         # if symbol is in starlight
         funcStr = symbolStr + r'\('
         try:
-            addrInStarlight = getSymAddrFromMap(funcStr, symbolStr)
+            addrInStarlight = getSymAddrFromMap("starlight", funcStr, symbolStr)
             # if no exception, then symbolStr is found is starlight
             resolvedAddr += addrInStarlight
-            return resolvedAddr
-        except:
+            return target, resolvedAddr
+        except NameError:
             pass
 
-    resolvedAddr += int(patchConfig["nso_load_addr"][target], 16)
-
-    # if symbolStr is already address
+    # if symbolStr is already an address
     try:
         resolvedAddr += int(symbolStr, 16)
-        return resolvedAddr
+        resolvedAddr += int(patchConfig["nso_load_addr"][target], 16)
+        return target, resolvedAddr
     except ValueError:
         pass
 
+    # search symbol in map
     regexStr = r'\w*'.join(re.findall(r'\w+', symbolStr))
     try:
-        resolvedAddr += getSymAddrFromMap(regexStr, symbolStr)
-    except:
+        resolvedAddr += getSymAddrFromMap(target, regexStr, symbolStr)
+    except NameError:
         print("genPatch.py error: cannot resolve", symbolStr)
         sys.exit(-1)
 
-    return resolvedAddr
+    return target, resolvedAddr
 
 def getPatchBin(target, patchAddress, patchValueStr):
     # bytes patch
@@ -136,8 +161,8 @@ def getPatchBin(target, patchAddress, patchValueStr):
     # asm patch
     branchNeedResolveMatch = re.match(r'([Bb][Ll]?\s+)([^\#]+$)', patchValueStr)
     if branchNeedResolveMatch:
-        patchValueStr = (branchNeedResolveMatch.group(1) + '#' +
-            hex(resolveAddress(target, branchNeedResolveMatch.group(2)) - patchAddress))
+        target, toAddr = resolveAddressAndTarget(target, branchNeedResolveMatch.group(2))
+        patchValueStr = (branchNeedResolveMatch.group(1) + '#' + hex(toAddr - patchAddress))
 
     ks = Ks(KS_ARCH_ARM64, KS_MODE_LITTLE_ENDIAN)
     encodedBytes, insCount = ks.asm(patchValueStr)
@@ -161,17 +186,18 @@ def addPatchFromFile(patchFilePath):
         fileLinesIter = iter(patchFile)
         isInMultiPatch = False
         while True:
+            # read next line
             if isInMultiPatch:
-                # multiPatch check already read new line
+                # multiPatch check already read new line; simply false the flag
                 isInMultiPatch = False
             else:
                 try:
                     line = next(fileLinesIter)
                 except StopIteration:
                     break
-
             line = line.split('/', 1)[0].strip()
 
+            # if is patch variable line, e.g. [version=310]
             patchVarLineMatch = re.match(r'\[(.+)\]', line)
             if patchVarLineMatch:
                 patchVarMatches = re.findall(r'(\w+)=(\w+)', patchVarLineMatch.group(1))
@@ -179,12 +205,17 @@ def addPatchFromFile(patchFilePath):
                     patchVars[match[0]] = match[1]
                 continue
 
+            # skip all lines not for our version
+            if patchVars["version"] != buildVersion and patchVars["version"] != PATCH_VERSION_ALL:
+                continue
+
+            # parse patches
             addressSplit = line.split(' ', 1)
             isInMultiPatch = addressSplit[0].endswith(':')
             if len(addressSplit) < 2 and not isInMultiPatch:
                 continue
             
-            patchAddress = resolveAddress(patchVars["target"],
+            patchTarget, patchAddress = resolveAddressAndTarget(patchVars["target"],
                 addressSplit[0] if not isInMultiPatch else addressSplit[0][:-1])
             patchContent = bytearray()
 
@@ -193,18 +224,17 @@ def addPatchFromFile(patchFilePath):
                     line = next(fileLinesIter).split('/', 1)[0]
                     ident = re.search(r'\s+', line).group()
                     while True:
-                        patchContent += getPatchBin(
-                            patchVars["target"], patchAddress + len(patchContent), line.strip())
+                        patchContent += getPatchBin(patchTarget, patchAddress + len(patchContent), line.strip())
                         line = next(fileLinesIter).split('/', 1)[0]
                         if not line.startswith(ident):
                             break
                 except StopIteration:
-                    addPatchToPatchlist(patchVars["target"], patchAddress, patchContent)
+                    addPatchToPatchlist(patchTarget, patchAddress, patchContent)
                     break
             else:
-                patchContent = getPatchBin(patchVars["target"], patchAddress, addressSplit[1])
+                patchContent = getPatchBin(patchTarget, patchAddress, addressSplit[1])
 
-            addPatchToPatchlist(patchVars["target"], patchAddress, patchContent)
+            addPatchToPatchlist(patchTarget, patchAddress, patchContent)
 
 if len(sys.argv) < 2:
     print('Usage: ' + sys.argv[0] + ' [version]')
@@ -212,15 +242,15 @@ if len(sys.argv) < 2:
 
 buildVersion = sys.argv[1]
 initConfig()
-mapFilePath = os.path.join("build" + buildVersion, "starlight" + buildVersion + ".map")
-with open(mapFilePath, 'r') as f:
-    mapFile = f.read()
-ipsOutDir = IPS_OUT_DIR_NAME.format(buildVersion)
+SLMapFilePath = os.path.join("build" + buildVersion, "starlight" + buildVersion + ".map")
+with open(SLMapFilePath, 'r') as f:
+    SLMapFile = f.read()
 
 for file in os.listdir(PATCH_DIR):
     if file.endswith(PATCH_EXTENSION):
         addPatchFromFile(os.path.join(PATCH_DIR, file))
 
+ipsOutDir = IPS_OUT_DIR_NAME.format(buildVersion)
 try:
     os.mkdir(ipsOutDir)
 except FileExistsError:
@@ -235,4 +265,4 @@ for nso in patchList:
             ipsFile.write(struct.pack('>H', patch.length))
             ipsFile.write(patch.content)
         ipsFile.write(IPS_EOF_MAGIC)
-    print("genPatch.py:", nso, "complete")
+    print("genPatch.py:", nso, "->", ipsOutPath, "successful")
