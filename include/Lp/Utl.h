6287b788e51bba6c4438a6e48f863283a88773d3
shadowninja108 <shadowninja0108@gmail.com>
2019-07-13 18:12:26 -0700

    Define dummy Param classes Add Lunchpack utils

--- include/Lp/Utl.h
+++ include/Lp/Utl.h
@@ -2,11 +2,25 @@
 
 #include "types.h"
 #include "Sys/ctrl.h"
+#include "sead/color.h"
+#include "Cmn/Def/Mode.h"
 
 namespace Lp
 {
     class Utl {
         public:
         static Sys::Ctrl* getCtrl(int);
+        static sead::Color4f hsb2rgb(sead::Vector3<float> const&);
+        static Cmn::Def::Mode getCurSceneId();
+        static char* getCurSceneName();
+        class ByamlIter {
+            
+        };
+        class ByamlWriter {
+            
+        };
+         class YamlWriter {
+            
+        };
     };
 };
